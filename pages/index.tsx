5c609666cbc4d33182d7c4041a31768c0007092d
Mathieu Welche <mathieuwelche@gmail.com>
2022-11-05 11:42:35 -0700

    add global types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
